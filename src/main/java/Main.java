c78f04d27520e451c671cc335fa9526bdac88426
Viacheslav <pubepoh@gmail.com>
2016-03-23 21:45:31 +0300

    counting messages with :peka:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -10,14 +12,29 @@ public class Main {
         Connection connect = new DbConnector().getConnection();
 
         Statement stmnt = null;
-        String query = "select message,uid from mysql.chat_message LIMIT 0, 10";
+        String query = "select message from mysql.chat_message LIMIT 0, 100";
+        int count = 0;
+
+        System.out.println("--------------------------");
 
         try{
             stmnt = connect.createStatement();
             ResultSet rs = stmnt.executeQuery(query);
+            String message;
+            String regex = ".*:peka:.*";
+            Pattern r = Pattern.compile(regex);
+
+
             while(rs.next()){
-                String message = rs.getString("message");
+                message = rs.getString("message");
                 System.out.println(message);
+                Matcher m = r.matcher(message);
+
+                while(m.find()){
+                    count++;
+                    System.out.println(count + " match!");
+                }
+
             }
         }catch(SQLException e){
             System.out.println("SQLException: " + e.getMessage());
