1660d86f4cf4dcb546092be1af20e6a2251f8cfd
Viacheslav <pubepoh@gmail.com>
2016-03-23 18:01:26 +0300

    established db connection via DbConnector

--- src/main/java/DbConnector.java
+++ src/main/java/DbConnector.java
@@ -2,22 +2,32 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Connector {
-    public static void main(String[] args){
+class DbConnector {
+
+    static void getConnection() {
+
+        String url = "jdbc:mysql://localhost:3306/mysql";
+        String user = "root";
+        String password = "root";
+
         Connection conn = null;
+
+        System.out.println("Connecting to database...");
 
         try{
 
-            conn = DriverManager.getConnection("jdbc::mysql://localhost/test?" + "user=root&password=root");
+            conn = DriverManager.getConnection(url,user,password);
             System.out.println("Success!");
 
         }catch(SQLException ex) {
 
+            System.out.println("Failed to connect.");
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
 
         }
+
     }
 
 }
