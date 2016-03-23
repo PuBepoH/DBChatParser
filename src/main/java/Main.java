148fbc55a4311af453eced1f41c01106aee81d1d
Viacheslav <pubepoh@gmail.com>
2016-03-23 19:57:11 +0300

    established db connection via DbConnector

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,27 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args){
 
-        DbConnector connect = new DbConnector();
-        DbConnector.getConnection();
+        Connection connect = new DbConnector().getConnection();
+
+        Statement stmnt = null;
+        String query = "select message,uid from mysql.chat_message LIMIT 0, 10";
+
+        try{
+            stmnt = connect.createStatement();
+            ResultSet rs = stmnt.executeQuery(query);
+            while(rs.next()){
+                String message = rs.getString("message");
+                System.out.println(message);
+            }
+        }catch(SQLException e){
+            System.out.println("SQLException: " + e.getMessage());
+        }
 
     }
 
