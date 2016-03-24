e10e5ffd66d805020f19e9a6f43126af97f7db53
Viacheslav <pubepoh@gmail.com>
2016-03-25 00:09:33 +0300

    memory allocation ptimization in MatchCounter

--- src/main/java/MatchCounter.java
+++ src/main/java/MatchCounter.java
@@ -3,14 +3,15 @@ import java.sql.SQLException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MatchCounter {
+class MatchCounter {
+
+    private int count = 0;
+    private String regex = ".*:peka:.*";
+    private String message;
+    private Pattern r = Pattern.compile(regex);
+    private Matcher m;
 
     int getCount(ResultSet rs) {
-
-        int count = 0;
-        String regex = ".*:peka:.*",message;
-        Pattern r = Pattern.compile(regex);
-        Matcher m;
 
         try{
             while (rs.next()){
