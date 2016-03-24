b9268ade6b503b32392e322d6ca22549b49239c5
Viacheslav <pubepoh@gmail.com>
2016-03-24 20:14:09 +0300

    problem with memory, while testing progressing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,12 +11,30 @@ public class Main {
 
         Connection connect = new DbConnector().getConnection();
 
-        int messages = 0;
+        long total = 8700700;
+        long done = 0,count =0;
+        double progress = 0;
+        Statement stmnt = null;
+        ResultSet rs = null;
 
-        messages = DbConnector.getCount(connect);
-        System.out.println("Total messages: " + messages);
+        //total = DbConnector.getCount(connect);
+        System.out.println("Total messages: " + total);
 
+        System.out.println("Progress:");
 
+        while(progress<100){
+            try{
+                stmnt = connect.createStatement();
+                rs = stmnt.executeQuery("select message from mysql.chat_message LIMIT 0, 1000");
+            }catch (SQLException ex){
+                System.out.println("SQLException in \"while\" cycle: " + ex);
+            }
+            done += 1000;
+            progress = (double)done/total*100;
+            if (progress<100) System.out.println(String.format("%.1f",progress) + "%");
+        }
+        System.out.println("100% - Done!");
+        System.out.println("Total count = " + count);
     }
 
 }
